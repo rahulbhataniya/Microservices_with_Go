cbd34c763942bea9d6bb08c20a4c2bb98b0af5ba
rahul_bhataniya <rahulbhataniya2468@gmail.com>
2024-06-03 10:05:53 +0530

    Day-3 completed

---
+++ product-api/go.mod
@@ -0,0 +1,3 @@
+module github.com/rahul-bhataniya/Microservices_with_Go
+
+go 1.21.3
---
+++ product-api/handlers/products.go
@@ -0,0 +1,42 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/rahul-bhataniya/Microservices_with_Go/data"
+)
+
+type Products struct {
+	l *log.Logger
+}
+
+func NewProducts(l *log.Logger) *Products {
+	return &Products{l}
+}
+
+func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	// handle the request for a list of products
+	if r.Method == http.MethodGet {
+		p.getProducts(rw, r)
+		return
+	}
+
+	// catch all
+	// if no method is satisfied return an error
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+// getProducts returns the products from the data store
+func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle GET Products")
+
+	// fetch the products from the datastore
+	lp := data.GetProducts()
+
+	// serialize the list to JSON
+	err := lp.ToJSON(rw)
+	if err != nil {
+		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+	}
+}
